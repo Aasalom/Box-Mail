e9ffbbe33d5971a12a856b7980d2ab51fde27223
aaSalom <anibalsalom@gmail.com>
2022-10-12 19:11:58 -0500

    Revert "mensaje"
    This reverts commit d117dc51cd818ee115bdc2437b194a91cb7ce084.

--- controlador.py
+++ controlador.py
@@ -1,94 +1,91 @@
 import sqlite3
 
-
-def ver_enviados(correo):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="select  m.asunto,m.mensaje,m.fecha, m.hora, u.nombreusuario  from usuarios u, mensajeria m where u.correo=m.id_usu_recibe and m.id_usu_envia='"+correo+"' order by fecha desc,hora desc"
-    cursor.execute(consulta)
-    resultado=cursor.fetchall()
+def ver_enviados(correo): #enviar un usuario y contraseña
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="select m.asunto,m.mensaje,m.fecha,m.hora,u.nombreusuario from usuarios u, mensajeria m where u.correo=m.id_usu_recibe and m.id_usu_envia='"+correo+"' order by fecha desc, hora desc"
+    cursor.execute(consulta) #ejecuta la consulta
+    resultado=cursor.fetchall() #la respuesta la envia a resultado
     return resultado
-
-def ver_recibidos(correo):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="select  m.asunto,m.mensaje,m.fecha, m.hora, u.nombreusuario  from usuarios u, mensajeria m where u.correo=m.id_usu_envia and m.id_usu_recibe='"+correo+"' order by fecha desc,hora desc"
-    cursor.execute(consulta)
-    resultado=cursor.fetchall()
+#--------------------------------------------------
+#--------------------------------------------------
+def ver_recibidos(correo): #enviar un usuario y contraseña
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="select m.asunto,m.mensaje,m.fecha,m.hora,u.nombreusuario from usuarios u, mensajeria m where u.correo=m.id_usu_envia and m.id_usu_recibe='"+correo+"' order by fecha desc, hora desc"
+    cursor.execute(consulta) #ejecuta la consulta
+    resultado=cursor.fetchall() #la respuesta la envia a resultado
     return resultado
-
-def validar_usuario(usuario, password):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="select *from usuarios where correo='"+usuario+"' and password='"+password+"' and estado='1'"
-    cursor.execute(consulta)
-    resultado=cursor.fetchall()
+#--------------------------------------------------
+#--------------------------------------------------
+def validar_usuario(usuario, password): #enviar un usuario y contraseña
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="select *from usuarios where correo='"+usuario+"' and password='"+password+"' and estado='1' " #crea la cadena de consulta
+    cursor.execute(consulta) #ejecuta la consulta
+    resultado=cursor.fetchall() #la respuesta la envia a resultado
     return resultado
-
-def lista_destinatarios(usuario):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="select *from usuarios where correo<>'"+usuario+"' "
-    cursor.execute(consulta)
-    resultado=cursor.fetchall()
+#--------------------------------------------------
+#--------------------------------------------------
+def lista_destinatarios(usuario): #enviar un usuario y contraseña
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="select *from usuarios where correo<>'"+usuario+"' " #crea la cadena de consulta
+    cursor.execute(consulta) #ejecuta la consulta
+    resultado=cursor.fetchall() #la respuesta la envia a resultado
     return resultado
-
-
-def actualizapass(password, correo):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="update usuarios set password='"+password+"' where correo='"+correo+"'"
-    cursor.execute(consulta)
-    db.commit()
+#--------------------------------------------------
+#--------------------------------------------------
+def actualizaPass(password, correo): 
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="update usuarios set password='"+password+"' where correo='"+correo+"' " #crea la cadena de consulta
+    cursor.execute(consulta) #ejecuta la consulta
+    db.commit() #para confirmar inserccion
     return "1"
-
-def registrar_mail(origen, destino, asunto, mensaje):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="insert into mensajeria (asunto,mensaje,fecha,hora,id_usu_envia,id_usu_recibe,estado) values ('"+asunto+"','"+mensaje+"',DATE('now'),TIME('now'),'"+origen+"','"+destino+"','0')"
-    cursor.execute(consulta)
-    db.commit()
+#--------------------------------------------------
+#--------------------------------------------------
+def registrar_mail(origen, destino, asunto, mensaje): #
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="insert into mensajeria (asunto,mensaje,fecha,hora,id_usu_envia,id_usu_recibe,estado) values('"+asunto+"','"+mensaje+"',DATE('now'),TIME('now'),'"+origen+"','"+destino+"','0') " #crea la cadena de consulta
+    cursor.execute(consulta) #ejecuta la consulta
+    db.commit() #para confirmar inserccion
     return "1"
-
-def registrar_usuario(nombre,correo, password,codigo):
+#--------------------------------------------------
+#--------------------------------------------------
+def registrar_usuario(nombre, correo, password, codigo): #
     try:
-        db=sqlite3.connect("mensajeria.s3db")
-        db.row_factory=sqlite3.Row
-        cursor=db.cursor()
-        consulta="insert into usuarios (nombreusuario,correo,password,estado,codigoactivacion) values ('"+nombre+"','"+correo+"','"+password+"','0','"+codigo+"')"
-        print("op 1")
-        cursor.execute(consulta)
-        print("op 2")
-        db.commit()
-        print("op 3")
-        return "Usuario Registrado Satisfactoriamente"
+        db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+        db.row_factory=sqlite3.Row #envia las cabeceras
+        cursor=db.cursor() #crea un apuntador
+        consulta="insert into Usuarios (nombreusuario,correo,password,estado,codigoactivacion) values ('"+nombre+"','"+correo+"','"+password+"','0','"+codigo+"') " #crea la cadena de consulta
+        cursor.execute(consulta) #ejecuta la consulta
+        db.commit() #para confirmar inserccion
+        return "Usuario registrado Satisfactoriamente"
     except:
-        return "ERROR!!! No es posible registrar al usuario debido a que el CORREO y/o NOMBRE DE USUARIO existen. Lo invitamos a modificar los campos pertinentes."
-        
-        
-    
-    
+        return "Error!!! Correo ya existente, verifique sus datos de nuevo."
 
-def activar_usuario(codigo):
-    db=sqlite3.connect("mensajeria.s3db")
-    db.row_factory=sqlite3.Row
-    cursor=db.cursor()
-    consulta="update usuarios set estado='1' where codigoactivacion='"+codigo+"'"
-    cursor.execute(consulta)
-    db.commit()
     
-    consulta2="select *from usuarios where codigoactivacion='"+codigo+"' and estado='1'"
-    cursor.execute(consulta2)
-    resultado=cursor.fetchall()
+#--------------------------------------------------
+#--------------------------------------------------
+def activar_usuario(codigo): #
+    db=sqlite3.connect("mensajeria.s3db") #se conecta a la base de datos
+    db.row_factory=sqlite3.Row #envia las cabeceras
+    cursor=db.cursor() #crea un apuntador
+    consulta="update usuarios set estado='1' where codigoactivacion='"+codigo+"' " #crea la cadena de consulta
+    cursor.execute(consulta) #ejecuta la consulta
+    db.commit() #para confirmar inserccion
+
+    consulta2="select *from usuarios where codigoactivacion='"+codigo+"' and estado='1' " #crea la cadena de consulta
+    cursor.execute(consulta2) #ejecuta la consulta
+    resultado=cursor.fetchall() #la respuesta la envia a resultado
     return resultado
-    
+
    
-
-
-    
